be7801334a1f2c4de4e90721e3b2da457b6c066e
sideeq12 <abdwaheed2018@gmail.com>
2020-12-08 11:32:19 -0800

    style

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ dotenv.config()
 
 
 const app = express();
+app.use(express.static("public"))
 app.use(BodyParser.urlencoded({extended : true}));
 app.set("view engine", "ejs")
 const key = process.env.API;
@@ -22,9 +23,12 @@ app.get("/", (req, res)=>{
         let name = title["2. name"]
         let symbol = title["1. symbol"]
         let fcas = title["3. fcas rating"]
-        console.log(fcas)
+        let Market = title["6. market maturity score"]
+        let Dev = title["5. developer score"]
+        let Facscore = title["4. fcas score"]
+        console.log(result)
         // console.log(result)
-        res.render("Home", {title : name, Symbol : symbol, FCAS : fcas})
+        res.render("Home", {title : name, Symbol : symbol, FCAS : fcas, Market : Market, Dev : Dev, Facscore : Facscore})
         
     })
 }) 
