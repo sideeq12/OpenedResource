5f93edf2abc2d203520668b05d9967cfac9a6f58
sideeq12 <abdwaheed2018@gmail.com>
2020-12-08 23:00:56 -0800

    btc

--- app.js
+++ app.js
@@ -15,23 +15,24 @@ const key = process.env.API;
 
 app.get("/", (req, res)=>{
     const url = "https://www.alphavantage.co/query?function=CRYPTO_RATING&symbol=BTC&apikey="+key
+    const url1 = "https://www.alphavantage.co/query?function=CRYPTO_RATING&symbol=ETH&apikey="+key
     https.get(url, (response)=>{
         // console.log(response.statusCode)
         response.on("data", (data)=>{
         const result = JSON.parse(data);
         let title = result["Crypto Rating (FCAS)"];
-        let name = title["2. name"]
-        let symbol = title["1. symbol"]
-        let fcas = title["3. fcas rating"]
-        let Market = title["6. market maturity score"]
-        let Dev = title["5. developer score"]
-        let Facscore = title["4. fcas score"]
-        let Utility = title["7. utility score"]
+        let BTCname = title["2. name"]
+        let BTCsymbol = title["1. symbol"]
+        let BTCfcas = title["3. fcas rating"]
+        let BTCMarket = title["6. market maturity score"]
+        let BTCDev = title["5. developer score"]
+        let BTCFacscore = title["4. fcas score"]
+        let BTCUtility = title["7. utility score"]
         console.log(result)
         // console.log(result)
-        res.render("Home", {title : name, Symbol : symbol, FCAS : fcas,
-             Market : Market, Dev : Dev, Facscore : Facscore,
-             Utility : Utility
+        res.render("Home", {Btitle : BTCname, BSymbol : BTCsymbol, BFCAS : BTCfcas,
+             BMarket : BTCMarket, BDev : BTCDev, BFacscore : BTCFacscore,
+             BUtility : BTCUtility
             })
         
     })
